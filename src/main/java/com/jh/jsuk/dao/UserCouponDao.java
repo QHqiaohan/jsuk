edbc2e0f6bb3e33e4dc2b7b63d2ddc49355f93bc
tj <tang123jian@126.com>
2018-06-22 11:40:36 +0800

    优惠券

--- src/main/java/com/jh/jsuk/dao/UserCouponDao.java
+++ src/main/java/com/jh/jsuk/dao/UserCouponDao.java
@@ -1,7 +1,10 @@
 package com.jh.jsuk.dao;
 
-import com.jh.jsuk.entity.UserCoupon;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jh.jsuk.entity.UserCoupon;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * <p>
@@ -12,5 +15,12 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * @since 2018-06-21
  */
 public interface UserCouponDao extends BaseMapper<UserCoupon> {
+
+    @Select({
+            "select * from js_user_coupon where coupon_id = #{couponId} and user_id = #{userId}"
+    })
+    @ResultMap("com.jh.jsuk.dao.UserCouponDao.BaseResultMap")
+    UserCoupon selectByCouponId(@Param("couponId") Integer couponId, @Param("userId") Integer userId);
+
 
 }
