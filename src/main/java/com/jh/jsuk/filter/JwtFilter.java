0fc2c7650a418c243f4796ac29ba5bec2cf00d69
xuchuruo <xuchuruo@qq.com>
2018-06-26 14:02:05 +0800

    get/post

--- src/main/java/com/jh/jsuk/filter/JwtFilter.java
+++ src/main/java/com/jh/jsuk/filter/JwtFilter.java
@@ -114,31 +114,31 @@ public class JwtFilter implements Filter {
                 // 首页模块
                 || servletPath.indexOf("/getAll") != -1
                 || servletPath.indexOf("/getAllBelow") != -1
-                //|| servletPath.indexOf("/getMoreInfo") != -1
-                //|| servletPath.indexOf("/getVipShop") != -1
-                //|| servletPath.indexOf("/hotel") != -1
-                // 商家店铺相关
-                //|| servletPath.indexOf("/moreShopList") != -1
-                //|| servletPath.indexOf("/show") != -1
-                //|| servletPath.indexOf("/getShopCategoryByShopId") != -1
-                // 专题精选
-                //|| servletPath.indexOf("/specialTheme") != -1
-                // 模块相关
-                //|| servletPath.indexOf("/modularPortal") != -1
-                // 便捷生活
-                //|| servletPath.indexOf("/lifeClass") != -1
-                // 商家端-获取分类列表
-                //|| servletPath.indexOf("/getModularList") != -1
-                // 用户端-商品类型相关API
-                //|| servletPath.indexOf("/goodsCategory") != -1
-                // 用户端-模块相关API
-                //|| servletPath.indexOf("/modularPortal") != -1
-                // 商家店铺相关
-                //|| servletPath.indexOf("/shop") != -1
-                // 商品相关
-                //|| servletPath.indexOf("/shopGoods") != -1
-                // 价格区间相关API
-                //|| servletPath.indexOf("/statisticsPrice") != -1
+            //|| servletPath.indexOf("/getMoreInfo") != -1
+            //|| servletPath.indexOf("/getVipShop") != -1
+            //|| servletPath.indexOf("/hotel") != -1
+            // 商家店铺相关
+            //|| servletPath.indexOf("/moreShopList") != -1
+            //|| servletPath.indexOf("/show") != -1
+            //|| servletPath.indexOf("/getShopCategoryByShopId") != -1
+            // 专题精选
+            //|| servletPath.indexOf("/specialTheme") != -1
+            // 模块相关
+            //|| servletPath.indexOf("/modularPortal") != -1
+            // 便捷生活
+            //|| servletPath.indexOf("/lifeClass") != -1
+            // 商家端-获取分类列表
+            //|| servletPath.indexOf("/getModularList") != -1
+            // 用户端-商品类型相关API
+            //|| servletPath.indexOf("/goodsCategory") != -1
+            // 用户端-模块相关API
+            //|| servletPath.indexOf("/modularPortal") != -1
+            // 商家店铺相关
+            //|| servletPath.indexOf("/shop") != -1
+            // 商品相关
+            //|| servletPath.indexOf("/shopGoods") != -1
+            // 价格区间相关API
+            //|| servletPath.indexOf("/statisticsPrice") != -1
 
             ////////////////////////////////////////////////
             //方便ios上架开放接口 TODO 上架后最好注掉
