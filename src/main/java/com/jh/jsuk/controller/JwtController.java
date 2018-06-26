2d41aa1161f3fd61615aecae439a2c1264f04288
luopanfeng <1295979574@qq.com>
2018-06-26 17:35:41 +0800

    修改筛选接口修改请求方式

--- src/main/java/com/jh/jsuk/controller/JwtController.java
+++ src/main/java/com/jh/jsuk/controller/JwtController.java
@@ -3,25 +3,26 @@ package com.jh.jsuk.controller;
 import com.jh.jsuk.utils.Result;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = {"令牌相关 不用管"})
-@RestController
+@RestController()
 public class JwtController {
     //过期
-    @RequestMapping("/overdue")
+    @RequestMapping(value = "/overdue", method = {RequestMethod.POST, RequestMethod.GET})
     public Result overdue() {
         return new Result().overdue();
     }
 
     //没有登陆
-    @RequestMapping("/noLogin")
+    @RequestMapping(value = "/noLogin", method = {RequestMethod.POST, RequestMethod.GET})
     public Result noLogin() {
         return new Result().noLogin();
     }
 
     //没有找到该用户
-    @RequestMapping("/nofound")
+    @RequestMapping(value = "/nofound", method = {RequestMethod.POST, RequestMethod.GET})
     public Result nofound() {
         return new Result().nofound();
     }
