d890545c302642850241473e24eaabbc034fe698
tj <tang123jian@126.com>
2018-06-27 16:17:40 +0800

    bug fix

--- src/main/java/com/jh/jsuk/controller/CollectGoodsController.java
+++ src/main/java/com/jh/jsuk/controller/CollectGoodsController.java
@@ -10,11 +10,10 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -25,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since 2018-06-25
  */
 @Api(tags = {"用户商品收藏:"})
-@Controller
+@RestController
 @RequestMapping("/collectGoods")
 public class CollectGoodsController {
 
