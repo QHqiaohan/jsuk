27d474f795196f27d349d3ecd096c816294dce48
tj <tang123jian@126.com>
2018-07-03 16:52:39 +0800

    合并

--- src/main/java/com/jh/jsuk/controller/GoodsBrandController.java
+++ src/main/java/com/jh/jsuk/controller/GoodsBrandController.java
@@ -2,19 +2,16 @@ package com.jh.jsuk.controller;
 
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.jh.jsuk.entity.GoodsBrand;
 import com.jh.jsuk.service.GoodsBrandService;
 import com.jh.jsuk.service.ShopGoodsService;
-import com.jh.jsuk.utils.Result;
+import com.jh.jsuk.utils.R;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * <p>
@@ -40,28 +37,6 @@ public class GoodsBrandController {
         wrapper.eq(GoodsBrand.STATUS, 1);
         return R.succ(goodsBrandService.selectList(wrapper));
     }
-
-    @ApiOperation("用户端-根据类型ID获取品牌列表")
-    @RequestMapping(value = "/getBrandByCategoryId", method = {RequestMethod.POST, RequestMethod.GET})
-    public Result getBrandByCategoryId(@ApiParam(value = "分类ID", required = true) Integer categoryId) {
-        List<GoodsBrand> goodsBrandList = goodsBrandService.selectList(new EntityWrapper<GoodsBrand>()
-                .eq(GoodsBrand.CATEGORY_ID, categoryId)
-                .eq(GoodsBrand.STATUS, 1)
-                .orderBy(GoodsBrand.SORT_ORDER, false));
-        return new Result().success(goodsBrandList);
-    }
-
-    /*@ApiOperation("用户端-商品类型里的-根据所选品牌ID查询商品")
-    @ApiImplicitParams(value = {
-            @ApiImplicitParam(name = "current", value = "当前页码", paramType = "query", dataType = "integer"),
-            @ApiImplicitParam(name = "size", value = "每页条数", paramType = "query", dataType = "integer"),
-    })
-    @RequestMapping(value = "/getShopGoodsByBrandId", method = {RequestMethod.POST, RequestMethod.GET})
-    public Result getShopGoodsByBrandId(@ApiParam(value = "品牌ID") Integer brandId, Page page) {
-        MyEntityWrapper<GoodsSalesPriceVo> ew = new MyEntityWrapper<>();
-        Page goodsPage = shopGoodsService.getShopGoodsByBrandId(page, ew, brandId);
-        return new Result().success(goodsPage);
-    }*/
 
 }
 
--- src/main/java/com/jh/jsuk/controller/GoodsCategoryController.java
+++ src/main/java/com/jh/jsuk/controller/GoodsCategoryController.java
@@ -1,30 +1,18 @@
 package com.jh.jsuk.controller;
 
 
-import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.baomidou.mybatisplus.plugins.Page;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.jh.jsuk.conf.Menu;
-import com.jh.jsuk.entity.Banner;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.jh.jsuk.entity.GoodsCategory;
 import com.jh.jsuk.service.BannerService;
 import com.jh.jsuk.service.GoodsCategoryService;
-import com.jh.jsuk.utils.MyEntityWrapper;
-import com.jh.jsuk.utils.Result;
+import com.jh.jsuk.utils.R;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
-
-import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -49,55 +37,6 @@ public class GoodsCategoryController {
         Wrapper<GoodsCategory> wrapper = new EntityWrapper<>();
         wrapper.eq(GoodsCategory.STATUS, 1);
         return R.succ(goodsCategoryService.selectList(wrapper));
-    }
-
-    @ApiOperation("用户端-获取商品所有类型")
-    @RequestMapping(value = "/getAllCategory")
-    public Result getAllCategory() {
-        /**
-         * 商品类型
-         */
-        List<GoodsCategory> goodsCategories = goodsCategoryService.selectList(new MyEntityWrapper<GoodsCategory>()
-                .orderBy(GoodsCategory.SORT_ORDER, false));
-        List<Menu> rootMenu = Lists.newArrayList();
-        goodsCategories.forEach(category -> {
-            Menu menu = new Menu();
-            menu.setId(String.valueOf(category.getId()));
-            menu.setIcon(category.getPic());
-            menu.setName(category.getName());
-            menu.setOrder(category.getSortOrder());
-            menu.setParentId(String.valueOf(category.getParentId()));
-            rootMenu.add(menu);
-        });
-        // 最后的结果
-        List<Menu> menuList = Lists.newArrayList();
-        // 先找到所有的一级菜单
-        for (int i = 0; i < rootMenu.size(); i++) {
-            // 一级菜单 parentId=0
-            if (StrUtil.equals(rootMenu.get(i).getParentId(), "0")) {
-                menuList.add(rootMenu.get(i));
-            }
-        }
-        // 为一级菜单设置子菜单，getChild是递归调用的
-        for (Menu menu : menuList) {
-            menu.setChildMenus(getChild(menu.getId(), rootMenu));
-        }
-        Map<String, Object> map = Maps.newHashMap();
-        map.put("goodsCategory", menuList);
-        /**
-         * 获取banner
-         */
-        List<Banner> bannerList = bannerService.selectList(new EntityWrapper<Banner>()
-                // 7=分类页banner
-                .eq(Banner.BANNER_LOCATION, 7)
-                .eq(Banner.IS_VALID, 1)
-                .orderBy(Banner.CREATE_TIME, false));
-        if (CollectionUtils.isEmpty(bannerList)) {
-            map.put("banner", null);
-        } else {
-            map.put("banner", bannerList);
-        }
-        return new Result().success(map);
     }
 
 }
