bb564c9a262997a2d62f58df842541e2390b6e10
qiaohan <m18804056940@163.com>
2018-07-19 11:34:43 +0800

    积分抵扣

--- src/main/java/com/jh/jsuk/controller/ShoppingCartController.java
+++ src/main/java/com/jh/jsuk/controller/ShoppingCartController.java
@@ -2,19 +2,23 @@ package com.jh.jsuk.controller;
 
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.jh.jsuk.entity.Coupon;
+import com.jh.jsuk.entity.ShopGoodsFullReduce;
 import com.jh.jsuk.entity.ShoppingCart;
+import com.jh.jsuk.entity.UserIntegral;
 import com.jh.jsuk.entity.vo.GoodsVo;
 import com.jh.jsuk.entity.vo.ShoppingCartVo;
-import com.jh.jsuk.service.ManagerUserService;
-import com.jh.jsuk.service.ShoppingCartService;
-import com.jh.jsuk.service.UserService;
+import com.jh.jsuk.service.*;
 import com.jh.jsuk.utils.MyEntityWrapper;
 import com.jh.jsuk.utils.Result;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -34,6 +38,10 @@ public class ShoppingCartController {
     UserService userService;
     @Autowired
     private ShoppingCartService shoppingCartService;
+    @Autowired
+    private UserIntegralService userIntegralService;
+    @Autowired
+    private ShopGoodsFullReduceService shopGoodsFullReduceService;
 
     @ApiOperation("加入购物车")
     @ApiImplicitParams(value = {
@@ -87,7 +95,9 @@ public class ShoppingCartController {
     @ApiOperation("根据购物车id删除购物车")
     @PostMapping("/del")
     public Result del(@ApiParam(value = "购物车id", required = true) @RequestParam Integer shoppingCartId, Integer userId) {
-        shoppingCartService.delete(new MyEntityWrapper<ShoppingCart>().eq(ShoppingCart.USER_ID, userId).eq(ShoppingCart.ID, shoppingCartId));
+        shoppingCartService.delete(new MyEntityWrapper<ShoppingCart>()
+                                      .eq(ShoppingCart.USER_ID, userId)
+                                      .eq(ShoppingCart.ID, shoppingCartId));
         return new Result().success();
     }
 
@@ -132,6 +142,66 @@ public class ShoppingCartController {
                return new Result().erro("购物车空空如也");
         }
         return new Result().success(shoppingCarts);
+    }
+
+    /*
+    给前台返回用户总积分
+    给前台返回满减对象
+    给前台返回积分抵扣规则
+     */
+    @ApiOperation("获取用户积分总数、满减对象、积分抵扣规则")
+    public Result getJfAndFullReduce(Integer userId,Integer [] shopIds){
+        Map<String,Object> map=new HashMap<>();
+
+        //查询用户总积分
+        UserIntegral userIntegral = userIntegralService.selectOne(new EntityWrapper<UserIntegral>()
+                                                                     .eq(UserIntegral.USER_ID, userId)
+        );
+        if(userIntegral==null || userIntegral.getIntegralNumber()==null){
+            map.put("jfCount",0);
+        }else{
+            map.put("jfCount",userIntegral.getIntegralNumber());
+        }
+
+        //查询满减对象
+        Map<Integer,ShopGoodsFullReduce> reduceMap=new HashMap<>();
+        for(Integer shopId:shopIds){
+            ShopGoodsFullReduce shopGoodsFullReduce = shopGoodsFullReduceService
+                                                     .selectOne(new EntityWrapper<ShopGoodsFullReduce>()
+                                                                .eq(ShopGoodsFullReduce.SHOP_ID, shopId)
+            );
+            //前台根据店铺id shopId取对应的满减对象
+            reduceMap.put(shopId,shopGoodsFullReduce);
+        }
+        map.put("shopGoodsFullReduceMap",reduceMap);
+
+        //查询积分抵扣规则
+
+
+        return new Result().success(map);
+    }
+
+
+    /**
+     * 用户-购物车-去结算
+     * 金额计算（折扣、优惠券、积分来计算订单价格）
+     * 计算订单金额
+     * 更新用户积分总数
+     */
+    /**
+     *
+     * @param cartPrice   前端传过来的购物车金额
+     * @param coupon      优惠券
+     * @param jfCount     可抵扣的积分数量
+     * @return
+     */
+    @ApiOperation("用户-购物车-去结算-计算订单金额")
+    @RequestMapping(value="/getOrderAmount",method ={RequestMethod.POST})
+    public Result getOrderAmount(double cartPrice,
+                                 @RequestBody Coupon coupon,
+                                 Integer jfCount){
+
+        return null;
     }
 }
 
