4fc16badea1d4fd215aa2e6124cb4d7521be187d
tj <tang123jian@126.com>
2018-08-10 12:04:29 +0800

    会员

---
+++ src/main/java/com/jh/jsuk/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.jh.jsuk.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 会员价格配置 前端控制器
+ * </p>
+ *
+ * @author tj
+ * @since 2018-08-10
+ */
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+}
+
---
+++ src/main/java/com/jh/jsuk/service/impl/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package com.jh.jsuk.service.impl;
+
+import com.jh.jsuk.entity.Member;
+import com.jh.jsuk.dao.MemberDao;
+import com.jh.jsuk.service.MemberService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 会员价格配置 服务实现类
+ * </p>
+ *
+ * @author tj
+ * @since 2018-08-10
+ */
+@Service
+public class MemberServiceImpl extends ServiceImpl<MemberDao, Member> implements MemberService {
+
+}
