93fce212c7564c0b8a0b4cfd6c09f0c9f7f1d64f
xyl <905756539@qq.com>
2018-08-21 17:25:16 +0800

    物流类型

---
+++ src/main/java/com/jh/jsuk/entity/LogisticsType.java
@@ -0,0 +1,25 @@
+package com.jh.jsuk.entity;
+
+/**
+ * Author:xyl
+ * Date:2018/8/21 16:24
+ * Description:
+ */
+public class LogisticsType {
+   private String pingying;
+
+   private String name;
+
+   public void setPingying(String pingying){
+       this.pingying=pingying;
+   }
+   public void setName(String name){
+       this.name=name;
+   }
+   public String getPingying(){
+       return pingying;
+   }
+   public String getName(){
+       return name;
+   }
+}
