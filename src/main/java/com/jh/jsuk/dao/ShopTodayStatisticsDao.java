ab9382aa85a9ed3e5f8077163093c5889a77551e
xyl <905756539@qq.com>
2018-08-24 18:08:18 +0800

    今日订单记录

--- src/main/java/com/jh/jsuk/dao/ShopTodayStatisticsDao.java
+++ src/main/java/com/jh/jsuk/dao/ShopTodayStatisticsDao.java
@@ -2,6 +2,7 @@ package com.jh.jsuk.dao;
 
 import com.jh.jsuk.entity.ShopTodayStatistics;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * <p>
@@ -12,6 +13,6 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  * @since 2018-06-20
  */
 public interface ShopTodayStatisticsDao extends BaseMapper<ShopTodayStatistics> {
-    ShopTodayStatistics getOneByshopId(String da1, String da2, Integer shopId);
+    ShopTodayStatistics getOneByshopId(@Param("da1")String da1, @Param("da2") String da2, @Param("shopId")Integer shopId);
 
 }
